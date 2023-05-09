bb60286bb8eb1f9a27d09724087e5ceac7f126a3
troyano97 <gygygy>
2023-05-09 12:13:50 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+const botonContador = document.querySelector ("#click");
+const pContador = document.querySelector("#contador");
+
+
+//let sumaClicks = botonContador.value;
+let Contador = 0;
+
+
+function contadorClick() {
+    contador + contador + 1
+ pContador.innerHTML = contador
+} 
